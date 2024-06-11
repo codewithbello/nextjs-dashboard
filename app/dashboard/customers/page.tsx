b995119509b0dc91f506689d52eb858fdc9c85aa
Castushtech <codewithbello@gmail.com>
2024-06-11 12:37:36 +0100

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Page() {
+  return (
+    // <div className="flex h-screen w-full bg-slate-800">Customers Page</div>
+    <div>Customers Page</div>
+  );
+}
